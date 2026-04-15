8cc923b63062e80fd62997361bf51faf2d06c372
Omar Hernandez Rey <hernandezreyomar@gmail.com>
2026-04-15 16:22:07 -0500

    Concept 3: Simple states with Class vs Functional components

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 function UseState({ name }) {
+  const [error, setError] = React.useState(false);
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
+
+      {error && (
+        <p>Error: el código es incorrecto</p>
+      )}
+
       <input placeholder="Código de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
